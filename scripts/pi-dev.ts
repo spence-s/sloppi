b1f85bf191a38fb5c1dabfa5db50a74c4e9983fa
Spencer Snyder <sasnyde2@gmail.com>
2026-07-29 23:33:01 -0400

    feat: fix up scripts

--- scripts/pi-dev.ts
+++ scripts/pi-dev.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-import process from 'node:process';
+import {copyFile, mkdir} from 'node:fs/promises';
+import os from 'node:os';
 import path from 'node:path';
+import process from 'node:process';
 import {$} from 'execa';
 
 const $$ = $({stdio: 'inherit'});
 
 const instance = 'pi-dev';
-
+const hostGitConfig = path.join(os.homedir(), '.gitconfig');
+const sharedGitConfigDirectory = path.join(os.homedir(), '.lima-git');
+const sharedGitConfig = path.join(sharedGitConfigDirectory, 'config');
 const configPath = path.join(import.meta.dirname, 'lima.yaml');
+
+await mkdir(sharedGitConfigDirectory, {recursive: true});
+await copyFile(hostGitConfig, sharedGitConfig);
 
 const {stdout: limaInstanceList} = await $`limactl list --quiet`;
 
